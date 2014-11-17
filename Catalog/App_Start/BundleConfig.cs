1e2d712932ad51d728e09c9b0cb599c5254872af
Anthony Fassett <fassetar@gmail.com>
2014-11-17 11:14:51 -0500

    Removed js files and kept min.js's
    This prevents anyone from modifying the root js files. You shouldn't
    need to modify them.

--- Catalog/App_Start/BundleConfig.cs
+++ Catalog/App_Start/BundleConfig.cs
@@ -7,21 +7,24 @@ namespace Catalog
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js",
-						"~/Scripts/jquery.unobtrusive*",
-						"~/Scripts/jquery.validate*"
+							"~/Scripts/jquery-{version}.min.js",
+							"~/Scripts/jquery.unobtrusive.min.js",
+							"~/Scripts/jquery.validate.min.js"
 						));
 
 			bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-						"~/Scripts/angular.min.js"));			
+							"~/Scripts/angular.min.js", 
+							"~/Scripts/angular-ui/ui-bootstrap-tpls.min.js",
+							"~/Scripts/angular-ui/ui-bootstrap.min.js"
+					   ));
 
 			bundles.Add(new ScriptBundle("~/bundles/controllers").Include(
 						"~/Scripts/Search.js"));
 
 			bundles.Add(new StyleBundle("~/Content/base").Include(
-				"~/Content/bootstrap.css",
-				"~/Content/site.css"
-				));
+							"~/Content/bootstrap.css",
+							"~/Content/site.css"
+						));
 		}
 	}
 }
