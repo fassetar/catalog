8603557dcd124f56365f4c2d1a6ee6e76040188b
Anthony Fassett <fassetar@gmail.com>
2014-11-17 10:50:25 -0500

    Bootstrap, AngularJs and much more.

--- Catalog/App_Start/BundleConfig.cs
+++ Catalog/App_Start/BundleConfig.cs
@@ -7,35 +7,21 @@ namespace Catalog
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-						"~/Scripts/jquery-ui-{version}.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/jquery-{version}.js",
 						"~/Scripts/jquery.unobtrusive*",
-						"~/Scripts/jquery.validate*"));
+						"~/Scripts/jquery.validate*"
+						));
+
+			bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+						"~/Scripts/angular.min.js"));			
 
 			bundles.Add(new ScriptBundle("~/bundles/controllers").Include(
 						"~/Scripts/Search.js"));
 
 			bundles.Add(new StyleBundle("~/Content/base").Include(
-				"~/Content/Site.css"));
-
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-						"~/Content/themes/base/jquery.ui.core.css",
-						"~/Content/themes/base/jquery.ui.resizable.css",
-						"~/Content/themes/base/jquery.ui.selectable.css",
-						"~/Content/themes/base/jquery.ui.accordion.css",
-						"~/Content/themes/base/jquery.ui.autocomplete.css",
-						"~/Content/themes/base/jquery.ui.button.css",
-						"~/Content/themes/base/jquery.ui.dialog.css",
-						"~/Content/themes/base/jquery.ui.slider.css",
-						"~/Content/themes/base/jquery.ui.tabs.css",
-						"~/Content/themes/base/jquery.ui.datepicker.css",
-						"~/Content/themes/base/jquery.ui.progressbar.css",
-						"~/Content/themes/base/jquery.ui.theme.css"));
-
+				"~/Content/bootstrap.css",
+				"~/Content/site.css"
+				));
 		}
 	}
 }
--- Catalog/Global.asax.cs
+++ Catalog/Global.asax.cs
@@ -17,7 +17,7 @@ namespace Catalog
 {    
 	public class MvcApplication : System.Web.HttpApplication
 	{
-        private static readonly string solrURL = ConfigurationManager.AppSettings["solrUrl"];
+		private static readonly string solrURL = ConfigurationManager.AppSettings["solrUrl"];
 		protected void Application_Start()
 		{
 			AreaRegistration.RegisterAllAreas();
@@ -26,49 +26,49 @@ namespace Catalog
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-            var solrServerUrl = ConfigurationManager.AppSettings["solrUrl"];
-            var solrConnection = new SolrConnection(solrServerUrl);
-            Startup.Init<Product>(solrConnection);
-            ModelBinders.Binders[typeof(SearchParameters)] = new SearchParametersBinder();
+			var solrServerUrl = ConfigurationManager.AppSettings["solrUrl"];
+			var solrConnection = new SolrConnection(solrServerUrl);
+			Startup.Init<Product>(solrConnection);
+			ModelBinders.Binders[typeof(SearchParameters)] = new SearchParametersBinder();
 
-            //var task = new System.Threading.Tasks.Task(AddInitialDocuments);
-            AddInitialDocuments();
+			//var task = new System.Threading.Tasks.Task(AddInitialDocuments);
+			AddInitialDocuments();
 		}
-        
-        public IEnumerable<SolrNet.ExtractField> fields = new SolrNet.ExtractField[] {new ExtractField("sku","PDF") };
+		
+		public IEnumerable<SolrNet.ExtractField> fields = new SolrNet.ExtractField[] {new ExtractField("sku","PDF") };
 
-       /// <summary>Adds some sample documents to Solr.</summary>
-        private void AddInitialDocuments()
-        {
-            try {
-               var solr = ServiceLocator.Current.GetInstance<ISolrOperations<Product>>();
-                solr.Delete(SolrQuery.All);
+	   /// <summary>Adds some sample documents to Solr.</summary>
+		private void AddInitialDocuments()
+		{
+			try {
+			   var solr = ServiceLocator.Current.GetInstance<ISolrOperations<Product>>();
+				solr.Delete(SolrQuery.All);
 
-                var connection = ServiceLocator.Current.GetInstance<ISolrConnection>();
-                foreach (var file in Directory.GetFiles(Server.MapPath("/exampledocs"), "*.xml")) {
-                    connection.Post("/update", File.ReadAllText(file, Encoding.UTF8));
-                }
+				var connection = ServiceLocator.Current.GetInstance<ISolrConnection>();
+				foreach (var file in Directory.GetFiles(Server.MapPath("/exampledocs"), "*.xml")) {
+					connection.Post("/update", File.ReadAllText(file, Encoding.UTF8));
+				}
 
-                foreach (var file in Directory.GetFiles(Server.MapPath("/exampledocs/"), "*.pdf", SearchOption.AllDirectories))
-                {
-                    var fileinfo = new FileInfo(file);
-                    using (FileStream fileStream = File.OpenRead(file))
-                    {
-                        var response = solr.Extract(
-                            new ExtractParameters(fileStream, fileinfo.FullName)
-                            {
-                                ExtractFormat = ExtractFormat.Text,
-                                ExtractOnly = false,
-                                Fields = fields
-                            });
-                    }
-                }
-                solr.Commit();
-                solr.BuildSpellCheckDictionary();                
-            } catch (SolrConnectionException) {
-                //Should really log this instead.
-                //throw new Exception(string.Format("Couldn't connect to Solr. Please make sure that Solr is running on '{0}' or change the address in your web.config, then restart the application.", solrURL));
-            }
-        }
+				foreach (var file in Directory.GetFiles(Server.MapPath("/exampledocs/"), "*.pdf", SearchOption.AllDirectories))
+				{
+					var fileinfo = new FileInfo(file);
+					using (FileStream fileStream = File.OpenRead(file))
+					{
+						var response = solr.Extract(
+							new ExtractParameters(fileStream, fileinfo.FullName)
+							{
+								ExtractFormat = ExtractFormat.Text,
+								ExtractOnly = false,
+								Fields = fields
+							});
+					}
+				}
+				solr.Commit();
+				solr.BuildSpellCheckDictionary();                
+			} catch (SolrConnectionException) {
+				//Should really log this instead.
+				//throw new Exception(string.Format("Couldn't connect to Solr. Please make sure that Solr is running on '{0}' or change the address in your web.config, then restart the application.", solrURL));
+			}
+		}
 	}
 }
