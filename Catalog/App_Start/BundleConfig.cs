c67c5a7b21c0910036313b0a5f85b287af2e1f8e
Anthony Fassett <fassetar@gmail.com>
2014-11-24 15:02:30 -0500

    Let Node handle it...

--- Catalog/App_Start/BundleConfig.cs
+++ Catalog/App_Start/BundleConfig.cs
@@ -5,29 +5,13 @@ namespace Catalog
 	/// <summary>Combine root styles in base and js in controllers bundle.</summary>
 	public class BundleConfig
 	{				
-		/// <summary>Always apply minification and bundling!</summary>
+		/// <summary>Always apply minification and bundling to CUSTOM styles and javascript!</summary>
 		/// <param name="bundles"></param>
 		public static void RegisterBundles(BundleCollection bundles)
-		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-							"~/Scripts/jquery-{version}.min.js",
-							"~/Scripts/jquery.unobtrusive.min.js",
-							"~/Scripts/jquery.validate.min.js"
-						));
-
-			bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-							"~/Scripts/angular.min.js", 
-							"~/Scripts/angular-ui/ui-bootstrap-tpls.min.js",
-							"~/Scripts/angular-ui/ui-bootstrap.min.js"
-					   ));
-
+		{						
+			//Everything else should be handle in bower.
 			bundles.Add(new ScriptBundle("~/bundles/controllers").Include(
-						"~/Scripts/Search.js"));
-
-			bundles.Add(new StyleBundle("~/Content/base").Include(
-							"~/Content/bootstrap.css",
-							"~/Content/site.css"
-						));
+						"~/Scripts/Search.js"));			
 		}
 	}
 }
