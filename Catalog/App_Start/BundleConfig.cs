fce57f69f63b0d34493f3309a9c4877e2662adbf
Anthony Fassett <fassetar@gmail.com>
2014-12-03 13:41:09 -0500

    Front-end connected.

--- Catalog/App_Start/BundleConfig.cs
+++ Catalog/App_Start/BundleConfig.cs
@@ -8,7 +8,10 @@ namespace Catalog
 		/// <summary>Always apply minification and bundling to CUSTOM styles and javascript!</summary>
 		/// <param name="bundles"></param>
 		public static void RegisterBundles(BundleCollection bundles)
-		{						
+		{
+			//TODO: tempt solution, until I think where to fit this.
+			bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/sites.css"));
+
 			//Everything else should be handle in bower.
 			bundles.Add(new ScriptBundle("~/bundles/controllers").Include(
 						"~/Scripts/Search.js"));			
