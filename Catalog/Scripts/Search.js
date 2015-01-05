7af56c4b726f386efbc85593cf6e97d2ca13e0af
Anthony Fassett <fassetar@gmail.com>
2015-01-05 00:57:51 -0500

    Styles and js update.

--- Catalog/Scripts/Search.js
+++ Catalog/Scripts/Search.js
@@ -1,4 +1,13 @@
-﻿function SearchController($scope) {    
+﻿(function (i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date(); a = s.createElement(o),
+	m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-41648493-8', 'auto');
+ga('send', 'pageview');
+
+function SearchController($scope) {
 	$scope.isLoadingSolr = false;
 	$scope.solrResult = null;
 	$scope.categoryFilter = '';	
