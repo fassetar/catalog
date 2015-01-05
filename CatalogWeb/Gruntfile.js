2cf92e07d48bd997e2923e04f52c047fb745bc3d
Anthony Fassett <fassetar@gmail.com>
2015-01-05 13:35:13 -0500

    Removed bower_components

---
+++ CatalogWeb/Gruntfile.js
@@ -0,0 +1,41 @@
+var path = require('path');
+module.exports = function (grunt) {
+	pkg: grunt.file.readJSON('package.json'),
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	
+	grunt.initConfig({
+		bower_concat: {
+			all: {
+				dest: 'build/main.js',
+				cssDest: 'build/main.css',
+				exclude: [
+					'jquery',	            
+				],			
+				bowerOptions: {
+						relative: false
+				}
+			}
+		},
+		bower: {
+			install: {
+				options: {
+                    targetDir: '../Catalog/',                    
+					layout: function (type, component) {
+						var renamedType = type;
+						if (type == 'js') renamedType = 'Scripts';
+						else if (type == 'css') renamedType = 'Content';
+						return path.join(renamedType);
+					},					              
+					install: true,
+					verbose: false,
+					cleanTargetDir: false,			
+					ignore: ["jquery"]                 
+				}
+			}
+		}
+	});
+	grunt.registerTask('default' , [		
+		'grunt-bower-concat',
+		'grunt-bower-installer'
+	]);
+};
