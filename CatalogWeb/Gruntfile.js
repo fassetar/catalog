83d4151f4fa11b320e091cda68009d7912b7d831
Anthony Fassett <fassetar@gmail.com>
2015-01-31 03:57:38 -0500

    Filtering out Jquery

--- CatalogWeb/Gruntfile.js
+++ CatalogWeb/Gruntfile.js
@@ -4,39 +4,15 @@ module.exports = function (grunt) {
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 	
 	grunt.initConfig({
-		bower_concat: {
-			all: {
-				dest: 'build/main.js',
-				cssDest: 'build/main.css',
-				exclude: [
-					'jquery',	            
-				],			
-				bowerOptions: {
-						relative: false
-				}
-			}
-		},
 		bower: {
 			install: {
-				options: {
-                    targetDir: '../Catalog/Exclude',
-                    cleanTargeDir: true,           
-					layout: function (type, component) {
-						var renamedType = type;
-                        if (type == 'js') renamedType = 'Scripts';
-                        else if (type == 'css') renamedType = 'Content';
-                        else if (type == 'woff') renameType = 'Content';;
-						return path.join(renamedType);
-					},					              
-					install: true,
-					verbose: false,
-					cleanTargetDir: false					               
+				options: {                                        					
+					install: true					
 				}
 			}
 		}
 	});
-	grunt.registerTask('default' , [		
-		'grunt-bower-concat',
-		'grunt-bower-installer'
+	grunt.registerTask('default' , [	
+		'bower'		
 	]);
 };
