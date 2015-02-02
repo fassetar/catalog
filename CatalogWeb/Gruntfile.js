60b0b0df36388df6c3544b5e78b35228e760a765
Anthony Fassett <fassetar@gmail.com>
2015-02-01 23:13:23 -0500

    Fixing files setup.

--- CatalogWeb/Gruntfile.js
+++ CatalogWeb/Gruntfile.js
@@ -1,14 +1,21 @@
 var path = require('path');
 module.exports = function (grunt) {
 	pkg: grunt.file.readJSON('package.json'),
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-	
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);	
 	grunt.initConfig({
 		bower: {
 			install: {
-				options: {                                        					
-					install: true					
-				}
+				options: {                    
+                     layout: function(type, component, source) {
+					  var renamedType = type;
+					  if (type == 'js') renamedType = 'javascripts';
+					  else if (type == 'css') renamedType = 'stylesheets';
+					  else renamedType = 'fonts';
+					  return path.join(renamedType);
+					}, 
+					cleanTargetDir: false,
+					targetDir: './lib'
+                }
 			}
 		}
 	});
