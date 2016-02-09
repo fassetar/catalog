690543ce25aa1826b2b3c33f8bdff1cb038aef85
fassetar <fassetar@gmail.com>
2016-02-09 10:56:30 -0500

    thanks better:http://codereview.stackexchange.com/questions/59678/simple-async-google-maps-initializer-with-angularjs

--- src/Catalog/Scripts/Search.js
+++ src/Catalog/Scripts/Search.js
@@ -1,22 +1,22 @@
 ﻿(function (i, s, o, g, r, a, m) {
-	i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-		(i[r].q = i[r].q || []).push(arguments)
-	}, i[r].l = 1 * new Date(); a = s.createElement(o),
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
 	m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 ga('create', 'UA-41648493-8', 'auto');
 ga('send', 'pageview');
 window.addEventListener('error', function (e) {
-	ga('send', 'event', 'Javascript Error', e.filename + ':  ' + e.lineno, e.message); 
-}); 
-var catalogApp = angular.module('catalogApp', ['ui.bootstrap', 'ui.grid']).controller('homeCtrl', function ($scope, $http) {
+    ga('send', 'event', 'Javascript Error', e.filename + ':  ' + e.lineno, e.message);
+});
+var catalogApp = angular.module('catalogApp', ['ui.bootstrap', 'ui.grid']).controller('homeCtrl', function ($scope, $http, Initializer) {
     //var showLabels = function (json) {
     //    var labels = json.feed;
     //    console.log(json);
     //    return json.feed.category;
     //};
 
-
+    console.log(Initializer.mapsInitialized);
     //Bootstrap Mobile
     $scope.isCollapsed = true;
     $scope.copyright = "© " + new Date().getFullYear();
@@ -89,75 +89,58 @@ var catalogApp = angular.module('catalogApp', ['ui.bootstrap', 'ui.grid']).contr
         enableVerticalScrollbar: 0
     };
     $http.get("Scripts/example.js").success(function (response) { $scope.myData = response; });
+    //'http://anthonyfassett.blogspot.com/feeds/posts/summary?max=results=0&alt=json-in-script&callback=initialize';
+}).factory('Initializer', function ($window, $q) {
 
-}).directive('lazyLoad', ['$window', '$q', function ($window, $q) {
-    function load_script() {
-        var s = document.createElement('script'); // use global document since Angular's $document is weak
-        s.src = 'http://anthonyfassett.blogspot.com/feeds/posts/summary?max=results=0&alt=json-in-script&callback=initialize';
-        document.body.appendChild(s);
-    }
-    function lazyLoadApi(key) {
-        var deferred = $q.defer();
-        $window.initialize = function () {
-            deferred.resolve();
-        };
-        if ($window.attachEvent) {
-            $window.attachEvent('onload', load_script);
-        } else {
-            $window.addEventListener('load', load_script, false);
-        }
-        return deferred.promise;
-    }
-    return {
-        restrict: 'E',
-        link: function (scope, element, attrs) {
-            // function content is optional
-            // in this example, it shows how and when the promises are resolved
-            if ($window.initialize) {
-                console.log('blog loaded');
-            } else {
-                lazyLoadApi().then(function () {
-                    console.log('promise resolved');
-                    if ($window.initialize) {
-                        console.log('blog loaded');
-                    } else {
-                        console.log('blog not loaded');
-                    }
-                }, function () {
-                    console.log('promise rejected');
-                });
-            }
-        }
+    //Google's url for async maps initialization accepting callback function
+    var asyncUrl = 'http://anthonyfassett.blogspot.com/feeds/posts/summary?max=results=0&alt=json-in-script&callback=',
+        mapsDefer = $q.defer();
+
+    //Callback function - resolving promise after maps successfully loaded
+    $window.googleMapsInitialized = mapsDefer.resolve; // removed ()
+
+    //Async loader
+    var asyncLoad = function (asyncUrl, callbackName) {
+        var script = document.createElement('script');
+        //script.type = 'text/javascript';
+        script.src = asyncUrl + callbackName;
+        document.body.appendChild(script);
     };
-}]);
+    //Start loading google maps
+    asyncLoad(asyncUrl, 'googleMapsInitialized');
 
+    //Usage: Initializer.mapsInitialized.then(callback)
+    return {
+        mapsInitialized: mapsDefer.promise
+    };
+});
 
 function SearchController($scope) {
-	$scope.isLoadingSolr = false;
-	$scope.solrResult = null;
-	$scope.categoryFilter = '';	
+    $scope.isLoadingSolr = false;
+    $scope.solrResult = null;
+    $scope.categoryFilter = '';
 
-	$scope.search = function () {
-		$scope.isLoadingSolr = true;
+    $scope.search = function () {
+        $scope.isLoadingSolr = true;
 
-		$.getJSON(
+        $.getJSON(
 			'/Home/Search/' + $scope.categoryFilter,
 			{
-				'q': $scope.searchTerm,
+			    'q': $scope.searchTerm,
 			},
 			function (data) {
-				$scope.$apply(function () {
-					$scope.isLoadingSolr = false;
-					$scope.solrResult = data;
-					$scope.myData = data;
-				});
+			    $scope.$apply(function () {
+			        $scope.isLoadingSolr = false;
+			        $scope.solrResult = data;
+			        $scope.myData = data;
+			    });
 			}
 		);
-		$scope.categoryFilter = '';
-	};
+        $scope.categoryFilter = '';
+    };
 
-	$scope.filterCategory = function (category) {
-		$scope.categoryFilter = category;
-		$scope.search();
-	};
+    $scope.filterCategory = function (category) {
+        $scope.categoryFilter = category;
+        $scope.search();
+    };
 }
